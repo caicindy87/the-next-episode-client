2dcddaa46eb94907df2aa933f85b9d786f9b93e0
SCDan0624 <dmayle1@hotmail.com>
2020-05-25 15:06:57 -0700

    Added Sign-In page form, not routed or Auth yet

--- src/containers/App.js
+++ src/containers/App.js
@@ -2,14 +2,23 @@ import React from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
 import NavBar from "../components/Navbar";
 import ShowContainer from "./ShowContainer";
+import LoginBox from "./LoginBox.js";
 
-function App() {
-  return (
-    <div className="App">
-      <NavBar />
-      <ShowContainer />
-    </div>
-  );
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      isLoginOpen: true,
+      isRegisterOpen: false,
+    };
+  }
+  render() {
+    return (
+      <div className="App">
+        <NavBar />
+        <ShowContainer />
+      </div>
+    );
+  }
 }
-
 export default App;
